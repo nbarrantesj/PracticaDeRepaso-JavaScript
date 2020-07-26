ec50a2ca850879bcf5c57787655404d3ceb29588
Nicole <nbarrantesj@gmail.com>
2020-07-25 22:49:43 -0600

    Ejercicio 2

---
+++ Ejercicio2/script/script.js
@@ -0,0 +1,10 @@
+let numero = parseInt(prompt('Ingrese un número: '));
+
+let suma = 0;
+
+for (let contador = 1; contador <= numero; contador++) {
+    suma += contador;
+}
+
+console.log(suma);
+ 
